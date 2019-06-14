36fcb1ac2183225b1e8e90f789fa46c1db701148
ryanthornton628 <ryanthornton628@gmail.com>
2019-06-14 15:52:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+var obj = { prop : 1 };
 
+var recipes = {};
+
+var newObj = {}
+
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+ var newObj = Object.assign({}, object)
+ return delete newObj.key
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object)
+  return delete object.key
+}
 
 
 
