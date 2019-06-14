7fd53635159d8cfd86be233f55915e9e8c944e41
ryanthornton628 <ryanthornton628@gmail.com>
2019-06-14 15:47:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
+ var newObj = Object.assign({}, object)
  return delete newObj.key
 }
 
